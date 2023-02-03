3bffc32a72bce0fa1807fd5978ac46e5b45b3670
zhalgas <b.zhalgas04@gmail.com>
2023-02-03 17:59:47 +0600

    assignment 3

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -70,7 +70,7 @@ func main() {
 	flag.StringVar(&cfg.smtp.host, "smtp-host", "smtp.office365.com", "SMTP host")
 	flag.IntVar(&cfg.smtp.port, "smtp-port", 587, "SMTP port")
 	flag.StringVar(&cfg.smtp.username, "smtp-username", "211510@astanait.edu.kz", "SMTP username")
-	flag.StringVar(&cfg.smtp.password, "smtp-password", "Askhat123452004", "SMTP password")
+	flag.StringVar(&cfg.smtp.password, "smtp-password", "**************", "SMTP password")
 	flag.StringVar(&cfg.smtp.sender, "smtp-sender", "211510@astanait.edu.kz", "SMTP sender")
 	flag.Parse()
 	logger := jsonlog.New(os.Stdout, jsonlog.LevelInfo)
